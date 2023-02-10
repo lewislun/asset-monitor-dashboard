e95bc396b3ce828efe7263e52c6cd4a6e760ce6f
Lewis Cheung <eu22@18m.dev>
2023-02-10 17:21:55 +0800

    feat: value by wallet type

--- src/routers/dashboard.js
+++ src/routers/dashboard.js
@@ -7,16 +7,23 @@ import env from '../../env.js'
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-	const [ totalValueByStateData, totalValueByCodeData, totalValueByChainData ] = await Promise.all([
+	const [
+		totalValueByStateData,
+		totalValueByCodeData,
+		totalValueByChainData,
+		totalValueByWalletTypeData,
+	] = await Promise.all([
 		await analytics.getTotalValue({ groupBy: 'state' }),
 		await analytics.getTotalValue({ groupBy: 'code' }),
 		await analytics.getTotalValue({ groupBy: 'chain' }),
+		await analytics.getTotalValue({ groupBy: 'tag', tagCategory: 'walletType' }),
 	])
 	res.render('pages/dashboard/summary', {
 		env,
 		totalValueByStateData: parseApexLabelValueData(totalValueByStateData, 'state', 'usdValue', { sortField: 'usdValue' }),
 		totalValueByCodeData: parseApexXYData(totalValueByCodeData, 'code', 'usdValue', { sortField: 'usdValue' }),
-		totalValueByChainData: parseApexLabelValueData(totalValueByChainData, 'chain', 'usdValue', { sortField: 'usdValue' })
+		totalValueByChainData: parseApexLabelValueData(totalValueByChainData, 'chain', 'usdValue', { sortField: 'usdValue' }),
+		totalValueByWalletTypeData: parseApexLabelValueData(totalValueByWalletTypeData, 'tagValue', 'usdValue'),
 	})
 })
 
